3d3f5493899e9aef1b0c01d20d5ea31ab7ac6440
guohang <gh722305@gmail.com>
2017-02-25 15:33:00 +0800

    update code

--- code/thread6.py
+++ code/thread6.py
@@ -5,3 +5,14 @@ local_school = threading.local()
 def process_student():
 	std = local_school.student
 	print('Hello, %s (in %s)' % (std, threading.current_thread().name))
+
+def process_thread(name):
+	local_school.student = name
+	process_student()
+
+t1 = threading.Thread(target=process_thread, args=('Alice',), name='Thread-A')
+t2 = threading.Thread(target=process_thread, args=('Bob',), name='Thread-B')
+t1.start()
+t2.start()
+t1.join()
+t2.join()
